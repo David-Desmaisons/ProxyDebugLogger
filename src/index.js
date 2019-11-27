da7d2ce061963da4225ade3f94f5005a688fe714
David <desmaisons_david@hotmail.com>
2019-11-26 22:53:03 -0300

    Almost there

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-function traceMethodCalls(obj) {
-  const handler = {
-    get(target, propKey) {
-      const origMethod = target[propKey];
-      if (typeof origMethod !== "function") {
-        return origMethod;
-      }
-      return function(...args) {
-        let result = origMethod.apply(target, args);
-        window.console.log(
-          propKey + JSON.stringify(args) + " -> " + JSON.stringify(result)
-        );
-        return result;
-      };
+function getFunctionHandler(propKey) {
+  return {
+    apply(target, thisArg, argumentsList) {
+      const result = target.apply(thisArg, argumentsList);
+      window.console.log(
+        propKey + JSON.stringify(argumentsList) + " -> " + JSON.stringify(result)
+      );
+      return result;
     }
   };
+};
+
+function getProxyFunction(propKey, original) {
+  const handler = getFunctionHandler(propKey);
+  return new Proxy(original, handler);
+}
+
+
+function getHandler() {
+  const cache = {};
+  return {
+    get(target, propKey) {
+      const res = cache[propKey];
+      if (res) {
+        return res;
+      }
+      const originalValue = target[propKey];
+      if (typeof originalValue !== "function") {
+        return originalValue;
+      }
+      const newFunction = getProxyFunction(propKey, originalValue);
+      cache[propKey] = newFunction;
+      return newFunction;
+    }
+  };
+}
+
+function traceMethodCalls(obj) {
+  const handler = getHandler();
   return new Proxy(obj, handler);
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,36 +3,34 @@ import { plugFakeWindow } from './Fake/FakeWindow';
 import { traceMethodCalls } from '../src/index';
 
 describe('traceMethodCalls()', async assert => {
+  const simpleObject = {
+    "one": 1,
+    "two": "two",
+    "three": [
+      "v1",
+      "v2",
+      "v3"
+    ]
+  };
+
+  assert({
+    given: 'an object without method',
+    should: 'return the same object',
+    actual: traceMethodCalls(simpleObject),
+    expected: simpleObject
+  });
+
+  const window = plugFakeWindow();
+
   {
-    const simpleObject = {
-      "one": 1,
-      "two": "two",
-      "three": [
-        "v1",
-        "v2",
-        "v3"
-      ]
-    };
-
-    assert({
-      given: 'an object without method',
-      should: 'return the same object',
-      actual: traceMethodCalls(simpleObject),
-      expected: simpleObject
-    });
-  }
-
-  {
-    const window = plugFakeWindow();
-
     const objectWithMethod = {
       id: (value) => value
     };
 
     const transformed = traceMethodCalls(objectWithMethod);
-    const { id } = transformed;
 
     ["abc", 1, null, undefined, ["a", "b"], []].forEach(value => {
+      const { id } = transformed;
       assert({
         given: 'a proxified object method',
         should: `return a method performing same transformation as original: ${value} => ${value} `,
@@ -42,13 +40,34 @@ describe('traceMethodCalls()', async assert => {
     });
 
     ["abc", 1, null, undefined, ["a", "b"], []].forEach(value => {
+      const { id } = transformed;
       id(value);
-      const expected = [`id${JSON.stringify([value])} -> ${JSON.stringify(value)}`];
       assert({
         given: 'a proxified object method',
         should: `log operation for entry ${value}`,
         actual: window.lastLog,
-        expected
+        expected: [`id${JSON.stringify([value])} -> ${JSON.stringify(value)}`]
+      });
+    });
+  }
+  {
+    class MyObject {
+      constructor(value) {
+        this.value = value;
+      }
+
+      getValue() { return this.value; }
+    }
+
+    ["value", 13].forEach(value => {
+      const object = new MyObject(value);
+      const transformed = traceMethodCalls(object);
+
+      assert({
+        given: 'a method',
+        should: `use the proxified object as this context and return ${value}`,
+        actual: transformed.getValue(),
+        expected: object.getValue()
       });
     });
   }
